45f38bf799157242e869f4da057b11e96b6f5e01
leizmo-frost <karanjalermodious123@gmail.com>
2024-02-29 16:35:18 +0300

    batches
    update on batches

---
+++ resources/views/batches/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+@section('content')
+
+
+<div class="card">
+  <div class="card-header">Students Page</div>
+  <div class="card-body">
+
+
+        <div class="card-body">
+        <h5 class="card-title">Name : {{ $batches->name }}</h5>
+        <p class="card-text">Course Name : {{ $batches->course_id }}</p>
+        <p class="card-text">Start Date : {{ $batches->start_date }}</p>
+  </div>
+
+    </hr>
+
+  </div>
+</div>
+@endsection
