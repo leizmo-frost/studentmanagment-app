8ce77e311f5ee249ca8267cfca152916febb64fe
leizmo-frost <karanjalermodious123@gmail.com>
2024-02-29 16:39:10 +0300

    batch
    route created

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -1,1 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use \App\Models\Batch;
+use Illuminate\View\View;
+
+class BatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): view
     {
+        $batches = Batch::all();
+        return view ('batches.index')->with('batches', $batches);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('batches.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $input = $request->all();
+        Batch::create($input);
+        return redirect('students')->with('flash_message', 'batch Addedd!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View
+    {
+        $batches = Batch::find($id);
+        return view('batches.show')->with('batches', $batches);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View
+    {
+        $batches = Batch::find($id);
+        return view('batches.edit')->with('batches', $batches);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $batch = Batch::find($id);
+        $input = $request->all();
+        $batch->update($input);
+        return redirect('batch')->with('flash_message', 'batch Updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        Batch::destroy($id);
+        return redirect('batch')->with('flash_message', 'batch deleted!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\BatchController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,6 @@ Route::resource("/students", StudentController::class);
 Route::resource("/teachers", TeacherController::class);
 
 Route::resource("/courses", CourseController::class);
+
+Route::resource("/batches", BatchController::class);
 
