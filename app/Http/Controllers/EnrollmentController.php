36dadb070f4ded7bc9a63ce346795e52ae009247
leizmo-frost <karanjalermodious123@gmail.com>
2024-03-01 15:23:41 +0300

    update on enrollment controller

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -14,7 +14,7 @@ class EnrollmentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $enrollments= Enrollment::all();
         return view ('enrollments.index')->with('enrollments', $enrollments);
@@ -23,7 +23,7 @@ class EnrollmentController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         return view('enrollments.create');
     }
@@ -31,7 +31,7 @@ class EnrollmentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $input = $request->all();
         Enrollment::create($input);
@@ -41,7 +41,7 @@ class EnrollmentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): View
     {
         $enrollments = Enrollment::find($id);
         return view('enrollments.show')->with('enrollments', $enrollments);
@@ -50,7 +50,7 @@ class EnrollmentController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
         $enrollments = Enrollment::find($id);
         return view('enrollments.edit')->with('enrollments', $enrollments);
@@ -59,7 +59,7 @@ class EnrollmentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): RedirectResponse
     {
         $enrollments = Enrollment::find($id);
         $input = $request->all();
@@ -70,7 +70,7 @@ class EnrollmentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
         Enrollment::destroy($id);
         return redirect('enrollments')->with('flash_message', 'enrollment deleted!');
--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -9,6 +9,6 @@ class Enrollment extends Model
 {
     protected $table = 'enrollment';
     protected $primaryKey = 'id';
-    protected $fillable = ['name', 'batch_id', 'student_id', 'join_date', 'fee'];
+    protected $fillable = ['name', 'enroll_no', 'batch_id', 'student_id', 'join_date', 'fee'];
     use HasFactory;
 }
