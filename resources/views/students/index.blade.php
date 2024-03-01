d47d4f13287efe88644fdc1ce7fe2f85b4f2a011
leizmo-frost <karanjalermodious123@gmail.com>
2024-03-01 09:55:27 +0300

    fixed student add button

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,7 +6,7 @@
             <h2>Student Application</h2>
         </div>
         <div class="card-body">
-            <a href="{{ url('/Students/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+            <a href="{{ url('/students/create') }}" class="btn btn-success btn-sm" title="Add New Student">
                 <i class="fa fa-plus" aria-hidden="true"></i> Add New
             </a>
             <br/>
