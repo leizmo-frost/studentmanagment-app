511d964333e54fed51bd16ac70c4bee9f3caf2b9
leizmo-frost <karanjalermodious123@gmail.com>
2024-03-01 13:05:07 +0300

    creation of enrollment

---
+++ database/migrations/2024_03_01_093504_create_enrollments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->string('enroll_no')->nullable();
+            $table->unsignedBigInteger('batch_id')->nullable()->unsigned();
+            $table->unsignedBigInteger('student_id')->nullable()->unsigned();
+            $table->date('join_date')->nullable();
+            $table->double('fee')->nullable();
+            $table->foreign('batch_id')->references('id')->on('batches')->onDelete('cascade');
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
