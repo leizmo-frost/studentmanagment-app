01a00121acb92639ba27c071382f1637f906c07a
leizmo-frost <karanjalermodious123@gmail.com>
2024-03-04 17:24:07 +0300

    added names in show page

--- resources/views/batches/show.blade.php
+++ resources/views/batches/show.blade.php
@@ -7,7 +7,7 @@
   <div class="card-body">
     <div class="card-body">
         <h5 class="card-title">Name: {{ $batch->name }}</h5>
-        <p class="card-text">Course Name: {{ $batch->course_id }}</p>
+        <p class="card-text">Course Name: {{ $batch->course->name }}</p>
         <p class="card-text">Start Date: {{ $batch->start_date }}</p>
     </div>
   </div>
