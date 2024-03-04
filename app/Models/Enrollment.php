f1c2ad1d4c498d2567269e47266daf33e0479c7b
leizmo-frost <karanjalermodious123@gmail.com>
2024-03-04 17:34:28 +0300

    added batch name and student name in enrollment

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -11,4 +11,14 @@ class Enrollment extends Model
     protected $primaryKey = 'id';
     protected $fillable = ['enroll_no', 'batch_id', 'student_id', 'join_date', 'fee'];
     use HasFactory;
+
+    public function student()
+    {
+        return$this->belongsTo (student::class);
+    }
+
+    public function batch()
+    {
+        return$this->belongsTo (batch::class);
+    }
 }
