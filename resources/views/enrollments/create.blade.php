b1d7c412be9bfca6a63c6a6b8721eb02bc060e38
leizmo-frost <karanjalermodious123@gmail.com>
2024-03-10 17:17:38 +0300

    a change in the for loop of the blade create template

--- resources/views/enrollments/create.blade.php
+++ resources/views/enrollments/create.blade.php
@@ -11,16 +11,16 @@
         <input type="text" name="enroll_no" id="enroll_no" class="form-control"><br>
         <label>Batch Id</label><br>
         <select name="batch_id" id="batch_id" class="form-control">
-        @foreach ($batches as $id =>name)
-        <option value="{{$id}}">{{ $name }}</option>
-        @endforeach
+          @foreach ($batches as $id => $name)
+          <option value="{{$id}}">{{ $name }}</option>
+          @endforeach
         </select>
 
         <label>Student Id</label><br>
             <select name="student_id" id="student_id" class="form-control">
-            @foreach ($students as $id =>name)
-            <option value="{{$id}}">{{ $name }}</option>
-            @endforeach
+              @foreach ($students as $id => $name)
+              <option value="{{$id}}">{{ $name }}</option>
+              @endforeach
             </select>
 
         <label>Join Date</label><br>
